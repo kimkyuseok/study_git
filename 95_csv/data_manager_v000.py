e99d7c6b0ff5e16bd66e409a3506bc388e946099
KyuSeok Kim <schnauzer109@gmail.com>
2024-06-14 16:59:33 +0900

    외주 리깅툴 -  버전관리및 파일체크 - 파일임포트 추가해서 정리 할 예정

---
+++ 95_csv/data_manager_v000.py
@@ -0,0 +1,16 @@
+# csv 파일을 가지고 원하는 데이터 가져 오는 연습
+import csv
+import os
+from datetime import datetime
+
+folder_path = r'D:\test\chackUS\study_git\95_csv\csv'
+file_name = 'data_3910_20240614.csv'
+full_file_path = os.path.join(folder_path, file_name)
+
+with open(full_file_path, 'r') as file:
+    reader = csv.DictReader(file)
+    data = [{k: v for k, v in row.items()} for row in reader]
+    sorted_data = sorted(data, key=lambda x: datetime.strptime(x['만기일'], '%Y/%m/%d'))
+    # 상위 두 개의 행 출력
+    for row in sorted_data[:2]:
+        print(row)
