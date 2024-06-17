ccf2e090d2c0ebad8c296a9b06603bf6ca279135
KyuSeok Kim <schnauzer109@gmail.com>
2024-06-17 14:29:06 +0900

    csv 가져오기 테스트

--- 95_csv/data_manager_v000.py
+++ 95_csv/data_manager_v000.py
@@ -11,7 +11,18 @@ full_file_path = os.path.join(folder_path, file_name)
 with open(full_file_path, 'r') as file:
     reader = csv.DictReader(file)
     data = [{k: v for k, v in row.items()} for row in reader]
-    sorted_data = sorted(data, key=lambda x: datetime.strptime(x['만기일'], '%Y/%m/%d'))
-    # 상위 두 개의 행 출력
-    for row in sorted_data[:2]:
-        print(row)
+    # print(data)
+    # 종목명에 '코스피200'이 들어간 항목을 찾아서 따로 추출
+    kospi_200_data = [row for row in data if '코스피 200' in row['기초자산명']]
+    end06_17_data = [row for row in kospi_200_data if '2024/06/17' in row['만기일']]
+    mire_data = [row for row in kospi_200_data if '미래에셋증권' in row['LP명']]
+    print(len(mire_data))
+    # '코스피200'을 포함하는 항목을 만기일 기준으로 정렬
+    # sorted_kospi_200_data = sorted(end06_17_data, key=lambda x: datetime.strptime(x['만기일'], '%Y/%m/%d'))
+    # # 상위 30개 항목 출력
+    codename = []
+    for row in mire_data:
+        # print(f'what name{row["종목명"]} : baseV {float(row["행사가격"])-374.8} : endV {row["최종종가"]} :: {row} ')
+        codename.append(row["종목코드"])
+    for i in codename:
+        print(i)
