fe499762235a361292ec0672a28689085f02a193
KyuSeok Kim <schnauzer109@gmail.com>
2024-06-18 17:10:37 +0900

    csv 데이터 처리 테스트 02

--- 95_csv/data_manager_v001.py
+++ 95_csv/data_manager_v001.py
@@ -9,17 +9,20 @@ folder_path = r'D:\test\chackUS\study_git\95_csv\csv'
 # file_name = 'data_2826_20240617.csv'
 file_name = 'data_1511_20240617.csv'
 full_file_path = os.path.join(folder_path, file_name)
-
+string_call = 'KOSPI200콜'
+string_put = 'KOSPI200풋'
+string_stock_name = '종목명'
+string_trading_volume = '거래량'
 with open(full_file_path, 'r') as file:
     reader = csv.DictReader(file)
     data = [{k: v for k, v in row.items()} for row in reader]
     # for i in data:
     #     print(i)
-    kospi_200_data = [row for row in data if 'KOSPI200콜' in row['종목명'] and row['거래량'] != '0']
-    kospi_200_data_a = sorted(kospi_200_data, key=lambda x: int(x['거래량']), reverse=True)
+    kospi_200_data = [row for row in data if string_call in row[string_stock_name] and row[string_trading_volume] != '0']
+    kospi_200_data_a = sorted(kospi_200_data, key=lambda x: int(x[string_trading_volume]), reverse=True)
     call = kospi_200_data_a
-    kospi_200_data = [row for row in data if 'KOSPI200풋' in row['종목명'] and row['거래량'] != '0']
-    kospi_200_data_b = sorted(kospi_200_data, key=lambda x: int(x['거래량']), reverse=True)
+    kospi_200_data = [row for row in data if string_put in row[string_stock_name] and row[string_trading_volume] != '0']
+    kospi_200_data_b = sorted(kospi_200_data, key=lambda x: int(x[string_trading_volume]), reverse=True)
     put = kospi_200_data_b
     if kospi_200_data_b:
         for i in kospi_200_data_b[:30]:
